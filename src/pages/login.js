fdf77637f6145c2d76dcfa42f1659a5dbcac6757
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-09-24 01:56:34 +0900

    Login finished

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 
 export default function Login() {
   const history = useHistory();
@@ -14,7 +15,18 @@ export default function Login() {
 
   const isInvalid = emailAddress === "" || password === "";
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = "Login - Instagram";
